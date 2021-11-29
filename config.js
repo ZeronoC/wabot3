e95c916992a25bb80d8935878c9b2efb5cf509a5
DafyBotz12 <92973411+DafyBotz12@users.noreply.github.com>
2021-11-29 13:09:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,33 +2,33 @@ global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
-  '62857139649630': { // put your number here
-    name: 'Owner Metro Bot/Creator games-wabot',
+  '62895323241456': { // put your number here
+    name: 'ɪɴɪ ᴅᴀғʏ',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: false, // if true this number will send if user use command /report or /join
     isCreator: true // if true this number will send if user use command /owner or /creator
   },
-  '6281390658325': { // put your number here
-    isDev: true, // if true this number will send if bot have bug and developer mode true
+  '6281379753850': { // put your number here
+    isDev: true, // if true this number will ᴅᴀғʏ ɴɪʜ ʙᴀɴɢ if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: false // if true this number will send if user use command /owner or /creator
   },
-  '6282256115584': { // put your number here
-    name: 'Dave',
+  '62895335209060': { // put your number here
+    name: 'ᴅɪᴏ ɢᴀɴᴢ',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: true, // if true this number will send if user use command /report or /join
     isCreator: false // if true this number will send if user use command /owner or /creator
   },
-  '60199782326': {
-    name: 'Adiixyz',
+  '60173227619': {
+    name: 'ɪᴢᴢᴀʜ ᴄᴀᴍᴛɪᴋ',
     isReport: true, // if true this number will send if user use command /report or /join
   },
-  '6285713964963': {
+  '6283144078722': {
     isReport: true
   },
   '13479805233': {} // ignore if you don't want anything, like name dll
 }
-global.mods = ['13479805233'] // Want some help?
+global.mods = ['62895323241456'] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -53,8 +53,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot Is The Best'
+global.packname = 'wa.me/6295323241456'
+global.author = 'ɪɴɪ ᴅᴀғʏ'
 
 global.multiplier = 39 // The higher, The harder levelup
 
